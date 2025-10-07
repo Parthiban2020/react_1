f0d266db01add127ae7a198b0919b0422fcd1020
Parthiban2020 <parthibank72@gmail.com>
2025-10-07 17:42:49 +0530

    Change_1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <header className="App-header">
+  <h1>Welcome Back to Our Hosted React App!</h1>
+  <p>This is the FINAL greeting for our React Web App. It's now live!</p>
+  <button>Click Me!</button>
+</header>
+    </div>
+  );
+}
+
+export default App;
